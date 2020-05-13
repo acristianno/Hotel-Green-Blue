eeeda274f111288d7cd580d4d545f20e5e7777c8
Anderson Cristiano F. Machado <65175553+acristianno@users.noreply.github.com>
2020-05-13 11:24:55 -0300

    Commit total da pagina

---
+++ pacote-download/Pagina Hotel Green blue/assets/js/menu.js
@@ -0,0 +1,13 @@
+document.querySelector('.menu__abrir').onclick = function() {
+    document.documentElement.classList.add('menu__ativo');
+};
+
+document.querySelector('.menu__fechar').onclick = function() {
+    document.documentElement.classList.remove('menu__ativo');
+};
+
+document.documentElement.onclick = function(event) {
+    if (event.target === document.documentElement) {
+        document.documentElement.classList.remove('menu__ativo');
+    }
+};
